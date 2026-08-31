eca32b33240d1092ffd59f042a9ccf851af50e68
adarshajudiya123 <adarshajudiyap.132737@marwadiuniversity.ac.in>
2026-08-31 22:53:49 +0530

    Add files via upload

---
+++ p3 operator.php
@@ -0,0 +1,37 @@
+<html>
+    <body>
+        <?php
+            $a=10;
+            $b=13;
+            echo "<h1>Arithmetic Operator</h1>";
+            echo "Addition of a and b is      : ".$a+$b."<br>";
+            echo "Subtraction of a and b is   : ".$a-$b."<br>";
+            echo "multiplication of a and b is: ".$a*$b."<br>";
+            echo "division of a and b is      : ".$a/$b."<br>";
+            echo "Modulas of a and b is       : ".$a%$b."<br>";
+            echo "<br><br>";
+            echo "<h1>RELATIONAL Operator</h1>";
+            if ($a>$b);
+                echo $a ." is greater than " . $b."<br>";
+            if ($a<$b);
+                echo $b ." is greater than ". $a."<br>";
+            if ($a>=$b);
+                echo "Both are equal"."<br>";
+            if ($a<=$b);
+                echo $a ." is less than or equal to ". $b."<br>";
+            if ($b<=$a);
+                echo $b ." is less than or equal to ". $a."<br>";
+            echo "<h1>ASSIGNMENT Operator</h1>";
+            $a=$b;
+            echo "a=".$a."<br>"."b=".$b."<br>";
+            echo "<h1>Bitwise Operator</h1>";
+            echo "Bitwise AND of a and b is : ".$a&$b."<br>";
+            echo "Bitwise OR of a and b is  : ".$a|$b."<br>";
+            echo "Bitwise XOR of a and b is : ".$a^$b."<br>";
+            
+
+
+        ?>
+
+    </body>
+</html>
